362aa1d5b38881727401ef9d7669a1ea6ce8e15a
Francisco Macedo <franciscovcbm@gmail.com>
2025-09-06 17:27:22 +0100

    Enhance index exports by adding schemas and lunch allowance components for improved modularity and access to configuration types.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from "./dependent-worker/simulator";
+export * from "./dependent-worker/schemas";
+export * from "./dependent-worker/lunch-allowance";
+export { LocationT, SituationCodesT, Situation, Condition, SituationUtils } from "./config/schemas";
