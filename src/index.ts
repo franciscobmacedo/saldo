ef86c3dd2c4e9eac4c8347fd942b523ae095b211
Francisco Macedo <franciscovcbm@gmail.com>
2025-09-09 16:38:33 +0100

    Export additional Situations type from config schemas in index.ts to enhance type definitions.

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from "./dependent-worker/simulator";
 export * from "./dependent-worker/schemas";
 export * from "./dependent-worker/lunch-allowance";
 export * from "./data/tax-tables-manifest";
-export { LocationT, SituationCodesT, Situation, Condition, SituationUtils } from "./config/schemas";
+export { LocationT, SituationCodesT, Situation, Condition, SituationUtils, Situations } from "./config/schemas";
